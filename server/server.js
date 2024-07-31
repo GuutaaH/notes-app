04098d675bd80b7e2fbe80f0ae2b4fbdee2fec71
GuutaaH <63647222+GuutaaH@users.noreply.github.com>
2024-07-31 14:55:23 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -20,22 +20,20 @@ app.use(express.json());
 app.use(cors())
 app.use(express.json())
 
-app.get('/todos/:userEmail', async (req, res) => {
-    const { userEmail } = req.params;
-    console.log(userEmail);
-    
-    if (!userEmail) {
-        return res.status(400).json({ error: 'User email is required.' });
-    }
+app.get('/todos/:userEmail', async (req,res) => {
 
-    try {
-        const todos = await pool.query('SELECT * FROM todos WHERE user_email = $1', [userEmail]);
-        res.json(todos.rows);
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ error: 'Internal Server Error' });
+    const {userEmail} = req.params
+    console.log(userEmail)
+    
+    try{
+const todos = await pool.query('SELECT * FROM todos WHERE user_email = $1', [userEmail])
+res.json(todos.rows)
     }
-});
+    catch (err){
+        console.log(err)
+
+    }
+})
 // create a new todo
 app.post('/todos', async(req,res) =>{
     try{
